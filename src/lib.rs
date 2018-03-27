e398c1cb2c8e27af7dfbe000fa89e079197f6d49
sitkevij <sitkevij@gmail.com>
2018-03-27 10:12:28 -0700

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -56,7 +56,7 @@ pub struct Line {
     /// ascii text
     pub ascii: Vec<char>,
     /// total bytes in Line
-    pub bytes: u64
+    pub bytes: u64,
 }
 /// Line implementation
 impl Line {
@@ -66,8 +66,8 @@ impl Line {
             offset: 0x0,
             hex_body: Vec::new(),
             ascii: Vec::new(),
-            bytes: 0x0
-            }   
+            bytes: 0x0,
+        }
     }
 }
 
@@ -79,7 +79,7 @@ pub struct Page {
     /// page body
     pub body: Vec<Line>,
     /// total bytes in page
-    pub bytes: u64
+    pub bytes: u64,
 }
 
 /// Page implementation
@@ -89,15 +89,15 @@ impl Page {
         Page {
             offset: 0x0,
             body: Vec::new(),
-            bytes: 0x0
-            }   
+            bytes: 0x0,
+        }
     }
 }
 
 /// offset column
-///  
-/// # Arguments 
-/// 
+///
+/// # Arguments
+///
 /// * `b` - offset value.
 pub fn offset(b: u64) -> String {
     format!("{:#08x}", b)
@@ -142,24 +142,52 @@ pub fn print_byte(b: u8, format: Format) {
     }
     // note, for color testing: for (( i = 0; i < 256; i++ )); do echo "$(tput setaf $i)This is ($i) $(tput sgr0)"; done
     match format {
-        Format::Octal => print!("{} ", ansi_term::Style::new().fg(ansi_term::Color::Fixed( color )).paint(hex_octal(b))),
-        Format::LowerHex => print!("{} ", ansi_term::Style::new().fg(ansi_term::Color::Fixed( color )).paint(hex_lower_hex(b))),
-        Format::UpperHex => print!("{} ", ansi_term::Style::new().fg(ansi_term::Color::Fixed( color )).paint(hex_upper_hex(b))),
-        Format::Binary => print!("{} ", ansi_term::Style::new().fg(ansi_term::Color::Fixed( color )).paint(hex_binary(b))),
+        Format::Octal => {
+            print!(
+                "{} ",
+                ansi_term::Style::new()
+                    .fg(ansi_term::Color::Fixed(color))
+                    .paint(hex_octal(b))
+            )
+        }
+        Format::LowerHex => {
+            print!(
+                "{} ",
+                ansi_term::Style::new()
+                    .fg(ansi_term::Color::Fixed(color))
+                    .paint(hex_lower_hex(b))
+            )
+        }
+        Format::UpperHex => {
+            print!(
+                "{} ",
+                ansi_term::Style::new()
+                    .fg(ansi_term::Color::Fixed(color))
+                    .paint(hex_upper_hex(b))
+            )
+        }
+        Format::Binary => {
+            print!(
+                "{} ",
+                ansi_term::Style::new()
+                    .fg(ansi_term::Color::Fixed(color))
+                    .paint(hex_binary(b))
+            )
+        }
         _ => print!("{}", "unk_fmt "),
     }
 }
 
-/// In most hex editor applications, the data of the computer file is 
-/// represented as hexadecimal values grouped in 4 groups of 4 bytes 
+/// In most hex editor applications, the data of the computer file is
+/// represented as hexadecimal values grouped in 4 groups of 4 bytes
 /// (or two groups of 8 bytes), followed by one group of 16 printable ASCII
-/// characters which correspond to each pair of hex values (each byte). 
+/// characters which correspond to each pair of hex values (each byte).
 /// Non-printable ASCII characters (e.g., Bell) and characters that would take
-/// more than one character space (e.g., tab) are typically represented by a 
+/// more than one character space (e.g., tab) are typically represented by a
 /// dot (".") in the following ASCII field.
-/// 
-/// # Arguments 
-/// 
+///
+/// # Arguments
+///
 /// * `matches` - Argument matches from command line.
 pub fn run(matches: ArgMatches) -> Result<(), Box<::std::error::Error>> {
     // let column_count: u64 = 0x0;
@@ -170,11 +198,11 @@ pub fn run(matches: ArgMatches) -> Result<(), Box<::std::error::Error>> {
         let buf_len = fs::metadata(file)?.len();
         let mut buf = BufReader::new(f);
         let mut format_out = Format::LowerHex;
-        
+
         if let Some(columns) = matches.value_of("cols") {
             column_width = columns.parse::<u64>().unwrap(); //turbofish
         }
-        
+
         if let Some(format) = matches.value_of("format") {
             // o, x, X, p, b, e, E
             match format {
@@ -205,7 +233,7 @@ pub fn run(matches: ArgMatches) -> Result<(), Box<::std::error::Error>> {
                 "g" => println!("a := [{}]byte{{", page.bytes),
                 _ => println!("unknown array format"),
             }
-            let mut i:u64 = 0x0;
+            let mut i: u64 = 0x0;
             for line in page.body.iter() {
                 print!("    ");
                 for hex in line.hex_body.iter() {
@@ -224,10 +252,10 @@ pub fn run(matches: ArgMatches) -> Result<(), Box<::std::error::Error>> {
                 "g" => println!("{}", "}"),
                 _ => println!("unknown array format"),
             }
-        } else {            
-            // Transforms this Read instance to an Iterator over its bytes. 
-            // The returned type implements Iterator where the Item is 
-            // Result<u8, R::Err>. The yielded item is Ok if a byte was 
+        } else {
+            // Transforms this Read instance to an Iterator over its bytes.
+            // The returned type implements Iterator where the Item is
+            // Result<u8, R::Err>. The yielded item is Ok if a byte was
             // successfully read and Err otherwise for I/O errors. EOF is mapped
             // to returning None from this iterator.
             // (https://doc.rust-lang.org/1.16.0/std/io/trait.Read.html#method.bytes)
@@ -242,14 +270,14 @@ pub fn run(matches: ArgMatches) -> Result<(), Box<::std::error::Error>> {
                     offset_counter += 1;
                     byte_column += 1;
                     print_byte(*hex, format_out);
-            
+
                     if *hex > 31 && *hex < 127 {
                         ascii_line.ascii.push(*hex as char);
                     } else {
                         ascii_line.ascii.push('.');
                     }
                 }
-                
+
                 if byte_column < column_width {
                     print!("{:<1$}", "", 5 * (column_width - byte_column) as usize);
                 }
@@ -269,17 +297,21 @@ pub fn run(matches: ArgMatches) -> Result<(), Box<::std::error::Error>> {
 }
 
 /// Buffer to array.
-/// 
+///
 /// (https://rustbyexample.com/primitives/array.html)
 /// (https://stackoverflow.com/questions/39464237/whats-the-idiomatic-way-reference-bufreader-bufwriter-when-passing-between-funct)
 /// (https://stackoverflow.com/questions/39935158/bufreader-move-after-for-loop-with-bufreader-lines)
-/// 
-/// # Arguments 
-/// 
+///
+/// # Arguments
+///
 /// * `buf` - Buffer to be read.
 /// * `buf_len` - Buffer length.
 /// * `column_width` - column width for output.
-pub fn buf_to_array(buf: &mut Read, buf_len: u64, column_width: u64) -> Result<Page, Box<::std::error::Error>> {
+pub fn buf_to_array(
+    buf: &mut Read,
+    buf_len: u64,
+    column_width: u64,
+) -> Result<Page, Box<::std::error::Error>> {
     let mut column_count: u64 = 0x0;
     let max_array_size: u16 = <u16>::max_value(); // 2^16;
     let mut page: Page = Page::new();
@@ -288,7 +320,7 @@ pub fn buf_to_array(buf: &mut Read, buf_len: u64, column_width: u64) -> Result<P
         let b1: u8 = b.unwrap();
         line.bytes += 1;
         page.bytes += 1;
-        line.hex_body.push( b1 );
+        line.hex_body.push(b1);
         column_count += 1;
         // println!("page.bytes =              {}", page.bytes);
         // println!("column_count =            {}", column_count);
@@ -317,39 +349,39 @@ mod tests {
     #[test]
     fn test_offset() {
         let b: u64 = 0x6;
-        assert_eq!( offset(b), "0x000006" );
-        assert_eq!( offset(b), format!("{:#08x}", b) );
+        assert_eq!(offset(b), "0x000006");
+        assert_eq!(offset(b), format!("{:#08x}", b));
     }
 
     /// hex octal, takes u8
     #[test]
     pub fn test_hex_octal() {
         let b: u8 = 0x6;
-        assert_eq!( hex_octal(b), "0o0006");
-        assert_eq!( hex_octal(b), format!("{:#06o}", b));
+        assert_eq!(hex_octal(b), "0o0006");
+        assert_eq!(hex_octal(b), format!("{:#06o}", b));
     }
 
     /// hex lower hex, takes u8
     #[test]
     fn test_hex_lower_hex() {
         let b: u8 = <u8>::max_value(); // 255
-        assert_eq!( hex_lower_hex(b), "0xff");
-        assert_eq!( hex_lower_hex(b), format!("{:#04x}", b));
+        assert_eq!(hex_lower_hex(b), "0xff");
+        assert_eq!(hex_lower_hex(b), format!("{:#04x}", b));
     }
 
     /// hex upper hex, takes u8
     #[test]
     fn test_hex_upper_hex() {
         let b: u8 = <u8>::max_value();
-        assert_eq!( hex_upper_hex(b), "0xFF");
-        assert_eq!( hex_upper_hex(b), format!("{:#04X}", b));
+        assert_eq!(hex_upper_hex(b), "0xFF");
+        assert_eq!(hex_upper_hex(b), format!("{:#04X}", b));
     }
 
     /// hex binary, takes u8
     #[test]
     fn test_hex_binary() {
         let b: u8 = <u8>::max_value();
-        assert_eq!( hex_binary(b), "0b11111111");
-        assert_eq!( hex_binary(b), format!("{:#010b}", b));
+        assert_eq!(hex_binary(b), "0b11111111");
+        assert_eq!(hex_binary(b), format!("{:#010b}", b));
     }
 }
